130e35a2d4d80f7b5f12e0df4ec8b12de062009f
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-06-27 12:38:32 -0700

    Improvement of tests by adding a link to the web driver in each auxiliary method from the current object

--- test_add_group2.py
+++ test_add_group2.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
 from group import Group
 
 def is_alert_present(wd):
@@ -17,67 +15,61 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
 
-    def open_home_page(self, wd):
-        # open home page
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def Login(self, wd, username, password):
-        # login
+    def Login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
-    def open_group_page(self, wd):
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
-
-
-    def create_group(self, wd, group):
+    def create_group(self, groups):
+        self.open_groups_page()
+        wd = self.wd
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_name").send_keys(groups.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_header").send_keys(groups.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("group_footer").send_keys(groups.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
-    def return_to_groups_page(self, wd):
-        # return to groups page
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def return_to_group_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def logout(self, wd):
-         # logout
-         wd.find_element_by_link_text("Logout").click()
-
-    def test_add_group(self):
+    def logout(self):
         wd = self.wd
-        self.open_home_page(wd)
-        self.Login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.create_group(wd, Group(name="gvgc", header="dsfdsfsfvzsd", footer="szdvfdxvzfdvd"))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_groups(self):
+        self.Login(username="admin", password="secret")
+        self.create_group(Group(name="gvgc", header="dsfdsfsfvzsd", footer="szdvfdxvzfdvd"))
+        self.return_to_group_page()
+        self.logout()
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.Login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
+        self.Login(username="admin", password="secret")
+        self.create_group(Group(name="", header="", footer=""))
+        self.return_to_group_page()
+        self.logout()
 
     def tearDown(self):
         self.wd.quit()

42927c32c7443c365dadc6336464475d665745f7
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-09 12:21:53 -0700

    Created some Tests for modification header, MiddleName and Last Name

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,3 +4,13 @@ def test_modify_contact_firstname(app):
     app.session.Login(username="admin", password="secret")
     app.group.modify_first_contact(new(First_name="New First Name"))
     app.session.logout()
+
+def test_modify_contact_middlename(app):
+    app.session.Login(username="admin", password="secret")
+    app.group.modify_first_contact(new(Middle_name="New Middle  Name"))
+    app.session.logout()
+
+def test_modify_contact_lastname(app):
+    app.session.Login(username="admin", password="secret")
+    app.group.modify_first_contact(new(Last_name="New Last_name  Name"))
+    app.session.logout()

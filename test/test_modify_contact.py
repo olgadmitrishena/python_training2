e340f4ced4344bacabac4e3eedf6bb72c0dbcbe2
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-10 01:10:58 -0700

    improved test to modify first contact and group

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,6 +1,8 @@
 from model.contact import new
 
 def test_modify_contact_firstname(app):
+    if app.group.count() == 0:
+        app.group.create_contact(new(First_name="test"))
     app.group.modify_first_contact(new(First_name="New First Name"))
 
 

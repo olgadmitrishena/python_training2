742e9a3f78038f0a44ad3c33b87e70bf768619a7
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-09 23:37:06 -0700

    increased reliability of tests after completing login / logout in SessionHelper

--- test/test_add_group2.py
+++ test/test_add_group2.py
@@ -5,7 +5,6 @@ from model.group import Group
 def test_add_groups(app):
     app.group.create(Group(name="gvgc", header="dsfdsfsfvzsd", footer="szdvfdxvzfdvd"))
    
-
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 

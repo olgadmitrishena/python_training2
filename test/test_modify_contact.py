b2a17f5508159d9bacb10aaefef0f0bcbbc9d0a7
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-10 01:40:46 -0700

    transitions between pages are carried out: "to carry out check - if the necessary page is already opened, the transition is not necessary"

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -7,8 +7,12 @@ def test_modify_contact_firstname(app):
 
 
 def test_modify_contact_middlename(app):
+    if app.group.count() == 0:
+        app.group.create_contact(new(Middle_name="test"))
     app.group.modify_first_contact(new(Middle_name="New Middle  Name"))
 
 
 def test_modify_contact_lastname(app):
+    if app.group.count() == 0:
+        app.group.create_contact(new(Last_name="test"))
     app.group.modify_first_contact(new(Last_name="New Last_name  Name"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -10,4 +10,6 @@ def test_modify_group_name(app):
 
 
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(header="test"))
     app.group.modify_first_group(Group(header="New header"))

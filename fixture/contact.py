181a32aab9122214e569a32a154db16372964b1b
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-16 13:24:56 -0700

    10. Optimized the transitions between pages

--- fixture/contact.py
+++ fixture/contact.py
@@ -29,7 +29,9 @@ class ContactHelper:
 
     def open_contact_page_to_add(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
+            wd.find_element_by_link_text("add new").click()
+
 
 
     def create_empty(self, contact):

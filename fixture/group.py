5d962f88df0001709647def6931cbe6bed2e959e
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-19 12:50:53 -0700

    Добавлена случайность в тест по модификации групп

--- fixture/group.py
+++ fixture/group.py
@@ -87,12 +87,14 @@ class GroupHelper:
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
 
+    def modify_first_group(self):
+        wd = self.app.wd
+        self.modify_group_by_index(0)
 
-
-    def modify_first_group(self, new_group_data):
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form

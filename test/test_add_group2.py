f798e26e2d7f8d24a400ad11c00f1c2603964c04
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-24 23:35:49 -0700

    Параметризация тесты для создания групп

--- test/test_add_group2.py
+++ test/test_add_group2.py
@@ -6,24 +6,25 @@ import string
 
 
 def random_string(prefix, maxlen):
-    simbols = string.ascii_letters + string.digits + " "
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+    simbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(simbols) for i in range(random.randrange(maxlen))])
 
 testdata = [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("header",20)),
-    Group(name="", header="", footer="")
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer",20)]
 ]
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
-    pass
-    # old_groups = app.group.get_group_list()
-    # app.group.create(group)
-    # assert len(old_groups) + 1 == app.group.count()
-    # new_groups = app.group.get_group_list()
-    # old_groups.append(group)
-    # assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+     old_groups = app.group.get_group_list()
+     app.group.create(group)
+     assert len(old_groups) + 1 == app.group.count()
+     new_groups = app.group.get_group_list()
+     old_groups.append(group)
+     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
--- test/test_add_new.py
+++ test/test_add_new.py
@@ -5,8 +5,8 @@ from model.contact import Contact
 
 def test_add_new_contact(app):
     old_contact_list = app.contact.get_contact_list()
-    contact_list = Contact(first_name="q", middle_name="w", last_name="e", title="r", company="t", adress="y", telephone="u", mobile="i", work="o",
-                       fax="p", e_mail="a", mailtwo="s", mailthree="d", homepage="f", adress2="g", homeadress="h", notestwo="j", byear="k", ayear="l")
+    contact_list = Contact(first_name="q", middle_name="w", last_name="e", title="r", company="t", adress="y", workphone="u", mobile="i",
+                       fax="p", e_mail="a", mailtwo="s", mailthree="d", homepage="f", address2="g", homeadress="h", notestwo="j", byear="k", ayear="l")
     app.contact.create(contact_list)
     assert len(old_contact_list) + 1 == app.contact.count()
     new_contact_list = app.contact.get_contact_list()

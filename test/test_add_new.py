e7a39ac70d5f2ac57618669176c6634d1cec1619
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-26 01:41:08 -0700

    15. Параметризация тестов для создания групп и контактов

--- test/test_add_new.py
+++ test/test_add_new.py
@@ -18,13 +18,12 @@ testdata = [Contact(firstname="", middlename="", lastname="", title="", company=
     for i in range(5)
 ]
 
-
 @pytest.mark.parametrize("contact", testdata, ids=[str(x) for x in testdata])
 def test_add_new_contact(app, contact):
     old_contact_list = app.contact.get_contact_list()
     app.contact.create(contact)
-    assert len(old_contact_list) + 1 == app.contact.count()
     new_contact_list = app.contact.get_contact_list()
+    assert len(old_contact_list) + 1 == app.contact.count()
     old_contact_list.append(contact)
     assert sorted(old_contact_list, key=Contact.id_max) == sorted(new_contact_list, key=Contact.id_max)
 

1cb772a9fe9969165a92ed70b6ebdfa77502b522
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-07-30 12:05:53 -0700

    19. Загрузка тестовых данных из файлов в формате JSON и параметризация тестов

--- test/test_add_new.py
+++ test/test_add_new.py
@@ -4,8 +4,8 @@ from model.contact import Contact
 
 
 
-def test_add_new_contact(app, data_contacts):
-    contact = data_contacts
+def test_add_new_contact(app, json_contacts):
+    contact = json_contacts
     old_contact_list = app.contact.get_contact_list()
     app.contact.create(contact)
     new_contact_list = app.contact.get_contact_list()
